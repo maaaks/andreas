c1eee60e2fc0796cd42521fdc6e78fda4a42bf5f
Max Alibaev <o@ololo.im>
2017-12-01 22:00:48 +0300

    Replaced setting post.user with creating UserPostRelation in test_commenting.

--- andreas/tests/test_commenting.py
+++ andreas/tests/test_commenting.py
@@ -2,7 +2,7 @@ from andreas.functions.process_event import process_event
 from andreas.functions.verifying import sign_post
 from andreas.models.event import Event
 from andreas.models.post import Post
-from andreas.models.relations import PostPostRelation
+from andreas.models.relations import PostPostRelation, UserPostRelation
 from andreas.models.server import Server
 from andreas.tests.andreastestcase import AndreasTestCaseWithKeyPair
 
@@ -13,16 +13,16 @@ class TestCommenting(AndreasTestCaseWithKeyPair):
         
         self.post = Post()
         self.post.server = self.server
-        self.post.user = self.abraham
         self.post.path = '/post1'
         self.post.data = {
             'body': 'How are you?',
         }
         self.post.save()
+        UserPostRelation.create(source=self.abraham, type='wrote', target=self.post)
         
         event = Event()
         event.server = 'aaa'
-        event.user = 'bernard@aaa'
+        event.authors = ['bernard@aaa']
         event.parent = 'aaa/post1'
         event.path = '/post1#c1'
         event.data = {'body': 'Good!'}
