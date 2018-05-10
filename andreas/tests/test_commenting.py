fe53737e83d90e229dd07db6c31924566ff8efe1
Max Alibaev <o@ololo.im>
2018-05-10 15:08:33 +0300

    Ref T8 Removed leading slash from paths in TestCommenting.

--- andreas/tests/test_commenting.py
+++ andreas/tests/test_commenting.py
@@ -13,7 +13,7 @@ class TestCommenting(AndreasTestCaseWithKeyPair):
         
         self.post = Post()
         self.post.server = self.server
-        self.post.path = '/post1'
+        self.post.path = 'post1'
         self.post.data = {
             'body': 'How are you?',
         }
@@ -24,7 +24,7 @@ class TestCommenting(AndreasTestCaseWithKeyPair):
         event.server = 'aaa'
         event.authors = ['bernard@aaa']
         event.parent = 'aaa/post1'
-        event.path = '/post1#c1'
+        event.path = 'post1#c1'
         event.data = {'body': 'Good!'}
         event.signatures = {
             'bernard@aaa': sign_post(event, self.bernard_keypair).hex(),
@@ -33,7 +33,7 @@ class TestCommenting(AndreasTestCaseWithKeyPair):
         process_event(event)
     
     def test_comment_created(self):
-        comment = Post.select().join(Server).where(Server.name == 'aaa', Post.path == '/post1#c1').get()
+        comment = Post.select().join(Server).where(Server.name == 'aaa', Post.path == 'post1#c1').get()
         PostPostRelation.get(
             PostPostRelation.source == comment,
             PostPostRelation.type == 'comments',
